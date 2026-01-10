2a381da8de5c453462783f94714edbbeaa3e16ba
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2026-01-10 14:09:20 +0700

    log: add header logging

--- src/main/java/com/roofiahmad/springstoreapp/common/LoggingFilter.java
+++ src/main/java/com/roofiahmad/springstoreapp/common/LoggingFilter.java
@@ -8,14 +8,29 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class LoggingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Request: " + request.getRequestURI());
+
+        System.out.println("--- Incoming Request ---");
+        System.out.println("Method: " + request.getMethod());
+        System.out.println("URI: " + request.getRequestURI());
+
+
+        Collections.list(request.getHeaderNames()).forEach(headerName -> {
+            String headerValue = request.getHeader(headerName);
+            System.out.println("Header: " + headerName + " = " + headerValue);
+        });
+
+
         filterChain.doFilter(request, response);
-        System.out.println("Response: " + response.getStatus());
+
+
+        System.out.println("Response Status: " + response.getStatus());
+        System.out.println("-------------------------");
     }
 }
