ca6cf0f0fca0bc87e5e02488a001e894281e131d
roofiahmadsidiq <roofiahmadsidiq@gmail.com>
2026-06-02 10:08:56 +0700

    debug: disable send email inside webhook

--- src/main/java/com/roofiahmad/springstoreapp/orders/OrderService.java
+++ src/main/java/com/roofiahmad/springstoreapp/orders/OrderService.java
@@ -8,15 +8,12 @@ import com.roofiahmad.springstoreapp.products.ProductRepository;
 import com.roofiahmad.springstoreapp.reviews.ProductReview;
 import com.roofiahmad.springstoreapp.reviews.ProductReviewRepository;
 import com.roofiahmad.springstoreapp.utils.Utils;
-import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Year;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -51,30 +48,30 @@ public class OrderService {
         System.out.println(paymentResult.getPaymentStatus() + " payment status");
         order.insertStatusHistory(paymentResult.getPaymentStatus(), "");
 
-        if(paymentResult.getPaymentStatus() == PaymentStatus.PAID) {
-            // send order confirmation email
-            var customer = order.getCustomer();
-            Map<String, Object> model = new HashMap<>();
-            model.put("customerName", customer.getName());
-            model.put("orderNumber", "REF-" + Year.now().getValue() + "-" + order.getId());
-            model.put("items", order.getItems().stream().map(p -> Map.of(
-                            "name", p.getProduct().getName(),
-                            "quantity", p.getQuantity(),
-                            "price", p.getTotalPrice()
-                    )
-            ).toList());
-            model.put("subtotal", order.getSubTotal());
-            model.put("shippingFee", order.getShippingFee());
-            model.put("vatAmount", order.getVatAmount());
-            model.put("totalAmount", order.getTotalPrice());
-
-            try {
-                emailService.sendOrderEmail(order.getCustomerEmail(), "ORDER-" + order.getId(), model);
-            } catch (MessagingException e) {
-                System.out.println(e.getMessage());
-                throw new RuntimeException(e);
-            }
-        }
+//        if(paymentResult.getPaymentStatus() == PaymentStatus.PAID) {
+//            // send order confirmation email
+//            var customer = order.getCustomer();
+//            Map<String, Object> model = new HashMap<>();
+//            model.put("customerName", customer.getName());
+//            model.put("orderNumber", "REF-" + Year.now().getValue() + "-" + order.getId());
+//            model.put("items", order.getItems().stream().map(p -> Map.of(
+//                            "name", p.getProduct().getName(),
+//                            "quantity", p.getQuantity(),
+//                            "price", p.getTotalPrice()
+//                    )
+//            ).toList());
+//            model.put("subtotal", order.getSubTotal());
+//            model.put("shippingFee", order.getShippingFee());
+//            model.put("vatAmount", order.getVatAmount());
+//            model.put("totalAmount", order.getTotalPrice());
+//
+//            try {
+//                emailService.sendOrderEmail(order.getCustomerEmail(), "ORDER-" + order.getId(), model);
+//            } catch (MessagingException e) {
+//                System.out.println(e.getMessage());
+//                throw new RuntimeException(e);
+//            }
+//        }
         orderRepository.save(order);
     }
 
